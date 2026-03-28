c8f7112e40f618a84e9f567b54388b72232de689
Floorball72 <57669878+Floorball72@users.noreply.github.com>
2026-03-28 17:33:17 +0100

    Delete MIGRATION_konto.sql

--- MIGRATION_konto.sql
+++
@@ -1,4 +0,0 @@
--- Migration: konto Spalte zu bestehender finanzen-Tabelle hinzufügen
--- Nur ausführen wenn SETUP_DATABASE.sql bereits gelaufen ist
-
-alter table finanzen add column if not exists konto text default 'Allgemein';
